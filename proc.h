6aa18f5cc4126762ff536c482f23f9c76865df48
Alayna Buysse <alaynagrace@comcast.net>
2021-02-23 21:57:51 -0600

    re

--- proc.h
+++ proc.h
@@ -49,9 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int tickets;                 // Tickets for lottery scheduler
-  int totalSleep;              // Total sleep ticks
-  int lastSleep;               // Last tick for sleep
+  int tickets;                 // Lottery scheduler tickets
+  int totalSleep;              // Total sleep
+  int lastSleep;               // Last sleep
 };
 
 // Process memory is laid out contiguously, low addresses first:
