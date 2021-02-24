8a26e479cc3457f80a405353c5f5c67cd815e968
Alayna Buysse <alaynagrace@comcast.net>
2021-02-23 22:55:58 -0600

    spin

--- makegraph.c
+++ makegraph.c
@@ -4,23 +4,19 @@
 #include "pstat.h"
 #include "fcntl.h"
 
-void spin()
-{
-  int i = 0;
-  int j = 0;
-  int k = 0;
-  for(i = 0; i < 50; ++i)
-    {
-      for(j = 0; j < 400000; ++j)
-        {
-	  k = j % 10;
-	  k = k + 1;
-	}
-    }
-}
+// void spin(){
+//   int i = 0;
+//   int j = 0;
+//   int k = 0;
+//   for(i = 0; i < 50; ++i){
+//         for(j = 0; j < 400000; ++j){
+// 	        k = j % 10;
+// 	        k = k + 1;
+// 	    }
+//     }
+// }
 
-int
-main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
   int numtickets[]={20,10,30};
   int pid_chds[3];
 
@@ -30,14 +26,13 @@ main(int argc, char *argv[]){
   int i;
   for(i=1;i<3;i++){
     pid_chds[i]=fork();
-    if(pid_chds[i]==0){
-      for (;;){
-	spin();
-      }
+    while(pid_chds[i]==0){
+        sleep(100);
+    //   for (;;){
+	//     spin();
+    //   }
     }
-    else{
-      settickets(numtickets[i]);
-    }
+    settickets(numtickets[i]);
   }
     
   struct pstat st;
@@ -45,12 +40,8 @@ main(int argc, char *argv[]){
   int hticks[3]={0,0,0};
   int lticks[3]={0,0,0};
 
-  printf(1,"pid:%d, pid:%d, pid:%d\n",pid_chds[0],pid_chds[1],pid_chds[2]);
-  printf(1,"tickets:%d, tickets:%d, tickets:%d\n",30,20,10);
-
   while(time<50){
     if(getpinfo(&st)!=0){
-      printf(1,"check failed: getpinfo\n");
       goto Cleanup;
     }
     
@@ -59,11 +50,10 @@ main(int argc, char *argv[]){
     for(i=0;i<3;i++){
       pid=pid_chds[i];
       for(j=0;j<NPROC;j++){
-	if(st.pid[j]==pid){
-      	hticks[i]=st.hticks[j];
-        lticks[i]=st.lticks[j];
-	  // printf(1,"pid:%d, tickets:%d, ticks:%d\n",pid,st.tickets[j],st.ticks[j]);
-	}
+	    if(st.pid[j]==pid){
+          	hticks[i]=st.hticks[j];
+            lticks[i]=st.lticks[j];
+	    }
       }
     }
 
@@ -71,15 +61,14 @@ main(int argc, char *argv[]){
    for(i=0;i<3;i++){
       printf(1,"High: %d, ",hticks[i]);
       printf(1,"Low: %d, ",lticks[i]);
-
     }
     printf(1,"\n");
-    
-    spin();
+    // spin();
+    sleep(100);
     time++;
   }
-    
- Cleanup:
+
+  Cleanup: 
   for (i = 0; pid_chds[i] > 0; i++){
     kill(pid_chds[i]);
   }
