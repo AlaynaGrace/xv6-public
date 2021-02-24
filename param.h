4ac954a1b6ce00b357b9199d2eff786ac02e3dc3
Alayna Buysse <alaynagrace@comcast.net>
2021-02-23 19:27:00 -0600

    params

--- param.h
+++ param.h
@@ -6,8 +6,14 @@
 #define NINODE       50  // maximum number of active i-nodes
 #define NDEV         10  // maximum major device number
 #define ROOTDEV       1  // device number of file system root disk
+#define PHYSTOP  0x1000000 // use phys mem up to here as free pool
+#define USERTOP  0xA0000 // end of user address space
 #define MAXARG       32  // max exec arguments
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
+#ifndef _PARAM_H_
+#define _PARAM_H_
+
+#endif // _PARAM_H_
