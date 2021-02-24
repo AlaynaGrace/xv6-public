d046987739073400d0ed915fab5c1cec1797c0a8
Alayna Buysse <alaynagrace@comcast.net>
2021-02-23 23:07:04 -0600

    try

--- makegraph.c
+++ makegraph.c
@@ -5,14 +5,14 @@
 #include "fcntl.h"
 
 int main(int argc, char *argv[]){
-  int numberOfTickets[]={20,10,30};
-  int pids[3];
+  int numberOfTickets[]={30,15,10};
+  int numberOfPids = 3;
+  int pids[numberOfPids];
 
   pids[0]=getpid();
   settickets(numberOfTickets[0]);
 
-  int i;
-  for(i=1;i<3;i++){
+  for(int i=1;i<numberOfPids;i++){
     pids[i]=fork();
     while(pids[i]==0){
         sleep(100);
@@ -20,14 +20,14 @@ int main(int argc, char *argv[]){
     settickets(numberOfTickets[i]);
   }
     
-  struct pstat st;
+  struct pstat ps;
   int time=0;
   int hticks[3]={0,0,0};
   int lticks[3]={0,0,0};
 
   while(time<50){
-    if(getpinfo(&st)!=0){
-        for (i = 0; pids[i] > 0; i++){
+    if(getpinfo(&ps)!=0){
+        for (int i = 0; pids[i] > 0; i++){
             kill(pids[i]);
         }
         while(wait() > -1);
@@ -37,17 +37,17 @@ int main(int argc, char *argv[]){
     
     int j;
     int pid;
-    for(i=0;i<3;i++){
+    for(int i=0;i<numberOfPids;i++){
       pid=pids[i];
       for(j=0;j<NPROC;j++){
-	    if(st.pid[j]==pid){
-          	hticks[i]=st.hticks[j];
-            lticks[i]=st.lticks[j];
+	    if(ps.pid[j]==pid){
+          	hticks[i]=ps.hticks[j];
+            lticks[i]=ps.lticks[j];
 	    }
       }
     }
 
-   for(i=0;i<3;i++){
+   for(int i=0;i<numberOfPids;i++){
       printf(1,"High: %d, \n",hticks[i]);
       printf(1,"Low: %d, \n",lticks[i]);
     }
